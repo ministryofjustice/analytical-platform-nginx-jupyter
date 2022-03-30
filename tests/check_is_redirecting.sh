abf1a6a3a8aa4665f05d055f1247626a2424b388
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-03-30 12:19:03 +0100

    Output status code information in test
    Co-authored-by: Louise Bowler <LouiseABowler@users.noreply.github.com>
    Co-authored-by: Gemma Hentsch <gemma@halfapenguin.com>

--- tests/check_is_redirecting.sh
+++ tests/check_is_redirecting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cmd=$(curl -s -o /dev/null -w "%{http_code}" localhost:80)
 if [[ $cmd -ne 302 ]]; then
-  echo "Test failed"
+  echo "Test failed because curl responded with a $cmd"
   exit 1
 else
   echo "Test passed"
